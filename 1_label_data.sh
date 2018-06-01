7a73d62352360c125dc9de879030cba645f4dc27
Charley Gros <chgroc@evans.neuro.polymtl.ca>
2018-06-01 15:09:42 -0400

    add conus label

--- 1_label_data.sh
+++ 1_label_data.sh
@@ -12,9 +12,9 @@
 cd t2_sag_cerv
 # set common orientation: AIL
 sct_image -i t2_sag_cerv.nii.gz -setorient AIL -o t2_sag_cerv.nii.gz
-# create Ponto-Medullary junction (50), top of C1 (1) and cervical cord disc labels, from C2-C3 (3) to C7-T1 (8).
+# create top of C1 (1) and cervical cord disc labels, from C2-C3 (3) to C7-T1 (8).
 # For instance, label 3 refers to C2-C3 disc.
-# Please create the labels in the spinal cord, at the level of the mid-vertebral body.
+echo "Please create the labels in the spinal cord, at the level of the mid-vertebral body."
 sct_label_utils -i t2_sag_cerv.nii.gz -create-viewer 1,2,3,4,5,6,7,8 -o label_discs.nii.gz
 cd ..
 
@@ -25,7 +25,7 @@ cd t2_sag_thor
 sct_image -i t2_sag_thor.nii.gz -setorient AIL -o t2_sag_thor.nii.gz
 # create thoracic cord disc labels, from C7-T1 (8) to T12-L1 (20)
 # For instance, label 9 refers to T1-T2 disc.
-# Please create the labels in the spinal cord, at the level of the mid-vertebral body.
+echo "Please create the labels in the spinal cord, at the level of the mid-vertebral body."
 sct_label_utils -i t2_sag_thor.nii.gz -create-viewer 8,9,10,11,12,13,14,15,16,17,18,19,20 -o label_discs.nii.gz
 cd ..
 
@@ -36,6 +36,7 @@ cd t2_sag_lumb
 sct_image -i t2_sag_lumb.nii.gz -setorient AIL -o t2_sag_lumb.nii.gz
 # create lumbar cord disc labels, from T12-L1 (20) to L5-S1 (25)
 # For instance, label 21 refers to L1-L2 disc.
-# Please create the labels in the spinal cord, at the level of the mid-vertebral body.
-sct_label_utils -i t2_sag_lumb.nii.gz -create-viewer 20,21,22,23,24,25 -o label_discs.nii.gz
+echo "Please create the labels in the spinal cord, at the level of the mid-vertebral body."
+echo "Label 100 refers to the conus."
+sct_label_utils -i t2_sag_lumb.nii.gz -create-viewer 20,21,22,23,24,25,100 -o label_discs.nii.gz
 cd ..
