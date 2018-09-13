f28790fac6553d628c30ca61c6ee45c08cb46a68
charleygros <charley.gros@gmail.com>
2018-09-13 16:20:00 +1000

    add scans list

--- parameters.sh
+++ parameters.sh
@@ -3,17 +3,26 @@
 # Set environment variables for the study.
  
 # path to input data (add "/" at the end)
-export PATH_DATA="/Users/chgroc/code/claudia_berlin/data_test/fnwlkn/data/"
+export PATH_DATA="/home/charley/data/claudia/"
  
 # list of subjects to analyse (add "/"" at the end)
 export SUBJECTS=(
-    "berlin_claudia_001/")
+    "berlin_claudia_000/"
+    "berlin_claudia_001/"
+    )
+
+# list of scan to analyse per subject (add "/"" at the end)
+export SCANS=(
+    "01_HWS/"
+    "02_BWS/"
+    "03_LWS/"
+    )
  
 # path to quality control (QC) (add "/"" at the end)
-export PATH_QC="/Volumes/projects/ms_seg/claudia/quality_control/"
+export PATH_QC="/home/charley/code/claudia/qc/"
  
 # results folder  (add "/"" at the end)
-export PATH_RESULTS="/Users/chgroc/code/claudia_berlin/data_test/fnwlkn/results/"
+export PATH_RESULTS="/home/charley/code/claudia/results/"
 
 # path to scripts (add "/" at the end)
-export PATH_SCRIPTS="/Users/chgroc/code/claudia_berlin/git/claudia/"
+export PATH_SCRIPTS="/home/charley/code/claudia/"
