82882adde12d526d2251a6d1f6942d37cda9a8b6
charleygros <charley.gros@gmail.com>
2018-09-13 16:25:40 +1000

    robust to vertebral availability + SCT 3.2.4

--- metrics.py
+++ metrics.py
@@ -1,27 +1,44 @@
 #!/usr/bin/env python
 #
-# This script extracts the computed metrics along the entire spinal cord.
+# This script extracts the computed metrics along the entire spinal cord,
+# and save them as excels or plots.
 #
 # Charley Gros 2018-05-17
+# Modified: 2018-09-12
 
 import sys
 import os
 import pandas as pd
-from msct_image import Image
 import numpy as np
+import operator
+import sys
 import matplotlib.pyplot as plt
 from matplotlib import rcParams
 
+import spinalcordtoolbox.image as msct_image
+
 rcParams.update({'font.size': 18})
-cmap = plt.get_cmap('Set2')
+CMAP_CERV = plt.get_cmap('Greens')(np.linspace(0.5, 1, 7))
+CMAP_THOR = plt.get_cmap('Blues')(np.linspace(0.5, 1, 12))
+CMAP_LUMB = plt.get_cmap('Reds')(np.linspace(0.5, 1, 5))
+
+SCALE_PLOT_DCT = {'csa': [0, 100],
+                    'RL_diameter': [0, 20],
+                    'AP_diameter': [0, 15],
+                    'eccentricity': [0, 1.1],
+                    'equivalent_diameter': [0, 15],
+                    'orientation': [-90, 90],
+                    'ratio_minor_major': [0, 1.1],
+                    'solidity': [0, 1.1],
+                    'symmetry': [0, 1.1]}
 
 
-def total_volume_cord(path_subject, subject_id, section_folder_name_lst, output_fname):
+def total_volume_cord(values, subject_id, output_fname):
     """
     Compute the volume of the entire spinal cord.
     :param path_subject: with a '/' at the end
     :param subject_id: subject folder name
-    :param section_folder_name_lst: list of the image folders (e.g. ['t2_cerv', 't2_thor', 't2_lumb'])
+    :param output_fname: filename of the csv file used to save the results
     """
     # check if output_fname file already exist
     if os.path.isfile(output_fname):
@@ -29,149 +46,161 @@ def total_volume_cord(path_subject, subject_id, section_folder_name_lst, output_
     else:
         tvc_pd = pd.DataFrame.from_dict({'subject_id': [], 'total_volume_cord': []})
 
-    # loop across the cord sections
-    tvc = 0.
-    for section_folder_name in section_folder_name_lst:
-        volume_pkl = os.path.join(path_subject, section_folder_name, 'metrics', 'csa_volume.pickle')
-        tvc += pd.read_pickle(volume_pkl)['MEAN across slices']
-    cvc = pd.read_pickle(os.path.join(path_subject, section_folder_name_lst[0], 'metrics', 'csa_volume.pickle'))['MEAN across slices']
+    tvc = np.sum(values)
 
     # if the subject already in the output_fname file, the value is updated, otherwise the new subject is appended
-    print subject_id, tvc_pd[tvc_pd['subject_id'] == subject_id].index[0]
     idx_pd = len(tvc_pd.index) if subject_id not in tvc_pd['subject_id'].values else tvc_pd[tvc_pd['subject_id'] == subject_id].index[0]
     tvc_pd.loc[idx_pd, 'subject_id'] = subject_id
     tvc_pd.loc[idx_pd, 'total_volume_cord'] = tvc
-    tvc_pd.loc[idx_pd, 'cervical_cord_volume'] = cvc
     tvc_pd.to_csv(output_fname)
 
 
-def extract_info_from_metric_file(csa_shape, path_folder, metric_name=None, file_prefixe=None):
-    """
-    Open the appropriated file (from sct_process_segmentation).
-    :param csa_shape: 'csa' or 'shape'
-    :param path_folder: with a '/' at the end
-    :param metric_name: one of the column name of the file *_shape.csv output by sct_process_segmentation (needed of csa_shape=='shape')
-    :param file_prefixe: prefixe of the file *_shape.csv output by sct_process_segmentation (needed of csa_shape=='shape')
-    :return : pandasdataframe
-    """
-    if csa_shape == 'csa':
-        csa_pkl = path_folder + 'csa_per_slice.pickle'
-        csa_section_pd = pd.read_pickle(csa_pkl)[['CSA (mm^2)', 'Slice (z)']]
-        return csa_section_pd.rename(columns={'CSA (mm^2)': 'raw_metric', 'Slice (z)': 'raw_z'})
-    elif csa_shape == 'shape':
-        shape_csv = path_folder + file_prefixe + '_shape.csv'
-        shape_section_pd = pd.read_csv(shape_csv)[['Unnamed: 0', metric_name]]
-        return shape_section_pd.rename(columns={metric_name: 'raw_metric', 'Unnamed: 0': 'raw_z'})
+def plot_profile(metric, pd2plot, subject_id, fname_out):
+    '''
+    Plots the profile of a given metric along the Superior-to-Inferior axis.
+    '''
+    toPlot_pd = pd2plot[['lvl', metric]]
+    toPlot_pd = toPlot_pd.dropna(subset=[metric])
+    toPlot_pd = toPlot_pd.reset_index(drop=True)
 
+    lvl_lst = list(set(toPlot_pd.lvl))
+    colors = np.vstack((CMAP_CERV, CMAP_THOR, CMAP_LUMB))
 
-def metric_profile(path_subject, subject_id, vert_info_dct, section_lst, output_fname, metric_name, y_lim_lst):
-    """
-    Compute the Superior-to-Inferior profile of a metric, and plot it per vertebral level.
-    :param path_subject: with a '/' at the end
-    :param subject_id: subject folder name
-    :param vert_info_dct:
-    :param section_lst: list of cord section sorted in the superiror to inferior axis
-    :param output_fname: filename of the saved profile plot
-    :param metric_name:
-    :param y_lim_lst: extrem values of the plot along the y axis
-    :return : pandas dataframe
-    """
-    metric_out_pd = pd.DataFrame.from_dict({'vert_level': [], metric_name: [], 'z_slice_raw_im': [], 'cord_section': []})
-
-    # loop over the different cord sections (e.g. cerv, thor, lumb)
-    for section in section_lst:
-        section_folder_name = vert_info_dct[section]['folder_name']
-        im_folder = os.path.join(path_subject, section_folder_name)
-
-        # Use the labeled segmentation file to match a z_slice_idx with a vertebral level value
-        seg_suffixe = '_seg_labeled.nii.gz' if os.path.isfile(os.path.join(im_folder, section_folder_name + '_seg_labeled.nii.gz')) else '_seg_manual_labeled.nii.gz'
-        labeled_seg_filename = os.path.join(im_folder, section_folder_name + seg_suffixe)
-        labeled_seg_data = Image(labeled_seg_filename).data
-
-        # extract values from file output by sct_process_segmentation
-        raw_csv_pd = extract_info_from_metric_file(csa_shape='csa' if metric_name == 'csa' else 'shape',
-                                                path_folder=os.path.join(im_folder, 'metrics', ''),
-                                                metric_name=metric_name,
-                                                file_prefixe=section_folder_name + seg_suffixe.split('_labeled')[0])
-
-        # loop over the vertebral levels in the current cord section
-        for vert_label in vert_info_dct[section]['vert_label'][:-1]:
-            idx_vert_section = vert_label - min(vert_info_dct[section]['vert_label']) + 1
-            z_vert_lst = sorted(list(set(np.where(labeled_seg_data == vert_label)[1])))[::-1]
-            metric_slice_pd = raw_csv_pd[raw_csv_pd['raw_z'].isin(z_vert_lst)][['raw_metric', 'raw_z']]
-            vert_name_lst = [vert_info_dct[section]['vert_section_initial'] + str(idx_vert_section) for vv in range(len(z_vert_lst))]
-            metric_vert_pd = pd.DataFrame.from_dict({'vert_level': vert_name_lst,
-                                                    metric_name: metric_slice_pd['raw_metric'].values,
-                                                    'z_slice_raw_im': metric_slice_pd['raw_z'].values,
-                                                    'cord_section': [section for vv in range(len(z_vert_lst))]})
-            metric_vert_pd = metric_vert_pd.sort_values(by=['z_slice_raw_im'], ascending=False)
-            metric_out_pd = pd.concat([metric_out_pd, metric_vert_pd])
-
-    # plot the profile along the superior to inferior axis
-    metric_out_pd['superior_to_inferior_idx'] = range(len(metric_out_pd.index))
-    vert_name_ordered = [vert_info_dct[section]['vert_section_initial'] + str(v) for section in ['cervical', 'thoracic', 'lumbar'] for v in range(1, len(vert_info_dct[section]['vert_label'])+1)]
-    colors = cmap(np.linspace(0, 1, len(vert_name_ordered)))
     plt.figure(figsize=(16, 6))
-    for i, (name, color) in enumerate(zip(vert_name_ordered, colors), 1):
-        metric_out_ver_pd = metric_out_pd[metric_out_pd['vert_level'] == name]
-        plt.scatter(metric_out_ver_pd['superior_to_inferior_idx'].values,
-                    metric_out_ver_pd[metric_name].values,
+    for i, (lvl, color) in enumerate(zip(lvl_lst, colors[:len(lvl_lst)]), 1):
+        toPlot_pd_cur = toPlot_pd[toPlot_pd['lvl'] == lvl]
+        plt.scatter(toPlot_pd_cur.index.values,
+                    toPlot_pd_cur[metric].values,
                     edgecolors='none', s=15,
-                    label=name, c=color)
-    plt.xlim([0, len(metric_out_pd.index)])
-    plt.ylim(y_lim_lst)
+                    label=str(lvl), c=color)
+    plt.xlim([0, len(toPlot_pd.index)])
+    plt.ylim(SCALE_PLOT_DCT[metric])
     plt.grid()
     plt.xlabel('slice along the Superior-to-Inferior axis')
-    plt.ylabel(metric_name)
-    plt.title(metric_name + ' along the Superior-to-Inferior axis - Subject: ' + subject_id)
+    plt.ylabel(metric)
+    plt.title('Subject: ' + subject_id)
     plt.legend(loc=9, bbox_to_anchor=(0.5, -0.15), ncol=12, fontsize=12)
-    plt.savefig(output_fname, bbox_inches="tight")
+    plt.savefig(fname_out, bbox_inches="tight")
 
-    return metric_out_pd[['cord_section', 'vert_level', 'z_slice_raw_im', 'superior_to_inferior_idx', metric_name]]
+
+def find_maxVert_perScan(data_folder):
+    '''
+    Opens each labelled image and find the max vertebral level available.
+    Return a dictionary with the scan_name as key, the max vert as value.
+    '''
+    scan_max_vert_dct = {}
+    for scan_foldname in os.listdir(data_folder):
+        path_labeled_cord_img = os.path.join(data_folder, scan_foldname, scan_foldname + '_seg_manual_labeled.nii.gz')
+        if not os.path.isfile(path_labeled_cord_img):
+            path_labeled_cord_img = os.path.join(data_folder, scan_foldname, scan_foldname + '_seg_labeled.nii.gz')
+        if os.path.isfile(path_labeled_cord_img):
+            scan_max_vert_dct[scan_foldname] = np.max(np.unique(msct_image.Image(path_labeled_cord_img).data))
+    return scan_max_vert_dct
+
+
+def extract_metrics_scans_asPanda(data_folder, scan_max_vert_dct, shape_metrics_lst):
+    '''
+    Loops across each scan results and extract the values, per vertebral level.
+    Returns a list of panda dataframes, one per scan with the following column: z_idx, lvl, csa and the shape_metrics_lst
+    '''
+    scan_results_pd_lst = []
+    for scan_foldname, max_vert in sorted(scan_max_vert_dct.items(), key=operator.itemgetter(1)):
+        path_scan_folder = os.path.join(data_folder, scan_foldname)
+        if os.path.isdir(path_scan_folder):
+            path_labeled_cord_img = os.path.join(path_scan_folder, scan_foldname + '_seg_manual_labeled.nii.gz')
+            if not os.path.isfile(path_labeled_cord_img):
+                path_labeled_cord_img = os.path.join(path_scan_folder, scan_foldname + '_seg_labeled.nii.gz')
+
+            path_shape_csv = os.path.join(path_scan_folder, 'metrics', scan_foldname + '_seg_manual_shape.csv')
+            if not os.path.isfile(path_shape_csv):
+                path_shape_csv = os.path.join(path_scan_folder, 'metrics', scan_foldname + '_seg_shape.csv')
+
+            path_csa_pickle = os.path.join(path_scan_folder, 'metrics', 'csa_per_slice.pickle')
+
+            if os.path.isfile(path_labeled_cord_img) and os.path.isfile(path_shape_csv) and os.path.isfile(path_csa_pickle):
+                labeled_cord_img = msct_image.Image(path_labeled_cord_img)
+                shape_pd = pd.read_csv(path_shape_csv)[[u'Unnamed: 0'] + shape_metrics_lst]
+                shape_pd = shape_pd.rename(columns={u'Unnamed: 0': 'z_idx'})
+                csa_pd = pd.read_pickle(path_csa_pickle)[[u'CSA (mm^2)', u'Slice (z)']]
+                csa_pd = csa_pd.rename(columns={u'Slice (z)': 'z_idx', u'CSA (mm^2)': 'csa'})
+
+                scan_results_tmp_dct = {}
+                for m in ['z_idx', 'csa', 'lvl'] + shape_metrics_lst:
+                    scan_results_tmp_dct[m] = []
+                scan_results_tmp_pd = pd.DataFrame.from_dict(scan_results_tmp_dct)
+
+                for zz in range(labeled_cord_img.dim[1]):
+                    lvl = np.max(np.unique(labeled_cord_img.data[:, zz, :]))
+                    if lvl:
+                        idx_tmp = len(scan_results_tmp_pd.index.values)
+                        scan_results_tmp_pd.loc[idx_tmp, 'z_idx'] = zz
+                        scan_results_tmp_pd.loc[idx_tmp, 'csa'] = csa_pd[csa_pd.z_idx == zz].csa.values[0]
+                        scan_results_tmp_pd.loc[idx_tmp, 'lvl'] = lvl
+                        for m_s in shape_metrics_lst:
+                            if len(shape_pd[shape_pd.z_idx == zz][m_s].values):
+                                scan_results_tmp_pd.loc[idx_tmp, m_s] = shape_pd[shape_pd.z_idx == zz][m_s].values[0]
+
+                scan_results_pd_lst.append(scan_results_tmp_pd)
+                if max_vert == max(scan_max_vert_dct.values()):
+                    break
+    return scan_results_pd_lst
+
+
+def remove_duplicated_vert(pd_lst, shape_metrics_lst):
+    '''
+    Because of the possible overlap between the scans of a subjects, we remove here the duplicated measures.
+    Returns one panda data frame covering the entire spinal cord.
+    '''
+    pd_out = pd_lst[0]
+    for scan_result_cur in pd_lst[1:]:
+        lvl_cur = scan_result_cur.lvl.values
+        lvl_saved = pd_out.lvl.values
+        intersec = list(set(lvl_cur) & set(lvl_saved))
+        print '\tOverlap detected for the vert. levels: ' + ' '.join([str(int(l)) for l in intersec])
+        if len(intersec):
+            for lvl_intersec in intersec:
+                lvl_intersec_pd_saved = pd_out[pd_out.lvl == lvl_intersec]
+                lvl_intersec_pd_cur = scan_result_cur[scan_result_cur.lvl == lvl_intersec]
+                if len(lvl_intersec_pd_saved.index.values) < len(lvl_intersec_pd_cur.index.values):
+                    pd_out = pd_out.drop(lvl_intersec_pd_saved.index)
+                    pd_out = pd_out.append(lvl_intersec_pd_cur, ignore_index=True)
+        pd_out = pd_out.append(scan_result_cur[scan_result_cur.lvl > max(lvl_saved)], ignore_index=True)
+
+    lvl_available = [int(l) for l in list(set(pd_out.lvl.values))]
+    if sorted(lvl_available) != range(min(lvl_available), max(lvl_available)+1):
+        sys.exit('Charley, there is a pb with the available vert: ' + ' '.join([str(l) for l in lvl_available]))
+
+    pd_out = pd_out.sort_values(['lvl', 'z_idx'], ascending=[True, False])
+
+    pd_out = pd_out[['lvl', 'csa'] + shape_metrics_lst + ['z_idx'] ]
+
+    return pd_out
 
 
 def run_main(path_data_folder, subject_id, path_results_folder):
     path_subject_folder = os.path.join(path_data_folder, subject_id)
-
-    cord_section_dct = {'cervical': {'folder_name': 't2_sag_cerv', 'vert_section_initial': 'C'},
-                        'thoracic': {'folder_name': 't2_sag_thor', 'vert_section_initial': 'T'},
-                        'lumbar': {'folder_name': 't2_sag_lumb', 'vert_section_initial': 'L'}}
-    for cord_section in cord_section_dct:
-        label_file = os.path.join(path_subject_folder, cord_section_dct[cord_section]['folder_name'], 'label_discs.nii.gz')
-        cord_section_dct[cord_section]['vert_label'] = list(set([label_value for label_value in list(np.unique(Image(label_file).data)) if label_value]))
-
-    cord_section_sorted_lst = ['cervical', 'thoracic', 'lumbar'] # sorted from superior to inferior
-
-    total_volume_cord_csv_filename = os.path.join(path_results_folder, '_total_volume_cord.csv')
-    total_volume_cord(path_subject=path_subject_folder,
-                      subject_id=subject_id.split('/')[0],
-                      section_folder_name_lst=[cord_section_dct[section]['folder_name'] for section in cord_section_sorted_lst],
-                      output_fname=total_volume_cord_csv_filename)
-
-    path_subject_result_folder = path_results_folder + subject_id
+    path_subject_result_folder = os.path.join(path_results_folder, subject_id)
     if not os.path.isdir(path_subject_result_folder):
         os.makedirs(path_subject_result_folder)
 
-    metric_dct = {'csa': [0, 100], 'RL_diameter': [0, 30], 'AP_diameter': [0, 20], 'ratio_minor_major': [0, 1]}
-    shape_pd = pd.DataFrame.from_dict({})
-    for metric in metric_dct:
-        profile_filename = path_subject_result_folder + metric + '_profile.png'
-        profile_pd = metric_profile(path_subject=path_subject_folder,
-                                        subject_id=subject_id,
-                                        vert_info_dct=cord_section_dct,
-                                        section_lst=cord_section_sorted_lst,
-                                        output_fname=profile_filename,
-                                        metric_name=metric,
-                                        y_lim_lst=metric_dct[metric])
-        if shape_pd.empty:
-            shape_pd = profile_pd
-        else:
-            shape_pd = pd.concat([shape_pd, profile_pd[metric]], axis=1)
+    shape_metrics_lst = ['AP_diameter', 'RL_diameter', 'eccentricity', 'equivalent_diameter', 'orientation', 'ratio_minor_major', 'solidity', 'symmetry']
 
-    shape_csv_filename = path_subject_result_folder + '_shape.csv'
-    shape_pd.to_csv(shape_csv_filename)
+    scan_max_vert_dct = find_maxVert_perScan(path_subject_folder)
+
+    scan_results_pd_lst = extract_metrics_scans_asPanda(path_subject_folder, scan_max_vert_dct, shape_metrics_lst)
+
+    scan_results_pd = remove_duplicated_vert(scan_results_pd_lst, shape_metrics_lst)
+    scan_results_pd.to_csv(os.path.join(path_subject_result_folder, 'measures.csv'), index=False)
+
+    total_volume_cord(values=scan_results_pd[(scan_results_pd.lvl >= 1.0)&(scan_results_pd.lvl <= 24.0)].csa.values,
+                        subject_id=subject_id,
+                        output_fname=path_results_folder + 'total_volume_cord.csv')
+
+    for metric in SCALE_PLOT_DCT.keys():
+        profile_filename = os.path.join(path_subject_result_folder, metric + '_profile.png')
+        plot_profile(metric, scan_results_pd, subject_id, profile_filename)
 
 
 if __name__ == '__main__':
     path_data, subject, path_results = sys.argv[1:]
-    run_main(path_data, subject, path_results)
+    run_main(path_data, subject[:-1], path_results)
