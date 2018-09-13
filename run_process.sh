b99615e52cbe9c2845a37fb958fa0de58f47fe8f
charleygros <charley.gros@gmail.com>
2018-09-13 16:19:21 +1000

    run process per scan

--- run_process.sh
+++ run_process.sh
@@ -13,8 +13,8 @@
 #
 # NB: add the flag "-x" after "!/bin/bash" for full verbose of commands.
 # Charley Gros 2018-05-13
- 
- 
+# modified: 2018-09-12
+
 # Load parameters
 source parameters.sh
  
@@ -23,11 +23,23 @@ PATH_PROCESS=`pwd`/$1
  
 # Loop across subjects
 for subject in ${SUBJECTS[@]}; do
-  # Display stuff
-  echo "Processing subject: ${subject}"
-  # go to subject folder
-  cd ${PATH_DATA}${subject}
-  export SUBJECT=${subject}
-  # run process
-  $PATH_PROCESS ${subject}
+	# Display stuff
+	echo "Processing subject: ${subject}"
+	# go to subject folder
+	cd ${PATH_DATA}${subject}
+	export SUBJECT=${subject}
+	if [ "$1" == "4_plot_store_values.sh" ]; then
+		$PATH_PROCESS ${subject}
+	else
+		for scan in ${SCANS[@]}; do
+			if [ -d "${scan}" ]; then
+				cd ${scan}
+				echo -e "\tProcessing scan: ${scan}"
+				export SCAN=${scan::-1}
+				# run process
+				$PATH_PROCESS ${subject}${scan}
+				cd ..
+			fi
+		done
+	fi
 done
